1e5aac648c7ed9f5a48328215ffb3ec1fb3fd442
十光年 <490983587@qq.com>
2019-06-22 18:11:54 +0800

    'tag_add.html,tag_list.html'

--- app/admin/views.py
+++ app/admin/views.py
@@ -21,3 +21,11 @@ def logout():
 @admin.route("/pwd/")
 def pwd():
     return render_template("admin/pwd.html")
+
+@admin.route("/tag/add/")
+def tag_add():
+    return render_template("admin/tag_add.html")
+
+@admin.route("/tag/list/")
+def tag_list():
+    return render_template("admin/tag_list.html")
