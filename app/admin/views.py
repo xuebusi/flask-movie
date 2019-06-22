8e4468677ea9d2c9ac8b60e6e86274f0ce441780
十光年 <490983587@qq.com>
2019-06-22 17:35:42 +0800

    'pwd.html'

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,15 +2,22 @@
 from . import admin
 from flask import render_template, redirect, url_for
 
+
 @admin.route("/")
 def index():
     return render_template("admin/index.html")
+
 
 @admin.route("/login/")
 def login():
     return render_template("admin/login.html")
 
+
 @admin.route("/logout/")
 def logout():
     return redirect(url_for("admin.login"))
 
+
+@admin.route("/pwd/")
+def pwd():
+    return render_template("admin/pwd.html")
