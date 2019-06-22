9e44c36b9cee2bf6b11c322bc7c9b1e63e06c940
十光年 <490983587@qq.com>
2019-06-22 19:14:32 +0800

    'movie_add.html,movie_list.html'

--- app/admin/views.py
+++ app/admin/views.py
@@ -29,3 +29,13 @@ def tag_add():
 @admin.route("/tag/list/")
 def tag_list():
     return render_template("admin/tag_list.html")
+
+@admin.route("/movie/add/")
+def movie_add():
+    return render_template("admin/movie_add.html")
+
+@admin.route("/movie/list/")
+def movie_list():
+    return render_template("admin/movie_list.html")
+
+
